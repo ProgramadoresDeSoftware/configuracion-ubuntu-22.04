1dd7b80d1cd66703992930a707e2932a8ef81e50
JOSE <diazvjosetomas@gmail.com>
2025-08-25 11:39:21 -0400

    (chore): add docker install sh

---
+++ docker-install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Instalando Docker..."
+sudo apt update -y
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update -y
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo usermod -aG docker $USER
+echo "✅ Docker instalado. Ejecuta 'newgrp docker' o cierra sesión y vuelve a entrar"
